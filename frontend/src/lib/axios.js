32cb02d95505537ef98543b487b40b95619dc7a1
hamza atig <hamzaatig2000@gmail.com>
2025-02-26 00:29:53 +0100

    fix code

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,9 +4,7 @@ console.log("mode : ", import.meta.mode);
 
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.mode === "development"
-      ? "http://localhost:4000/api"
-      : "https://backend-wr1y.onrender.com/api",
+    import.meta.mode === "development" ? "http://localhost:4000/api" : "/api",
   withCredentials: true, // send cookies to the server
 });
 
