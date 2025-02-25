ff7a3963a5280be65e435b6ce6166e829a2bd769
hamza atig <hamzaatig2000@gmail.com>
2025-02-26 00:08:38 +0100

    fix code

--- backend/server.js
+++ backend/server.js
@@ -45,10 +45,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
-
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
   connectDB();
